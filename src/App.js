1747d3a9706d247d39a0e86f728dbe0e64b8bbd0
Sajida <sajidamustafa22@gmail.com>
2023-01-19 19:18:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React,{useState} from 'react';
+import './styles.css';
+const App=()=>{
+
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async()=>{
+
+   // console.log('before');
+
+     const response= await fetch("https://reqres.in/api/users?page=1");
+   // const response= await fetch("https://api.github.com/users");
+    const jsonresponse = await response.json();
+
+    setUsers(jsonresponse.data);
+    console.log(users);
+  };
+
+  return(
+ <div className="App">
+
+      <h1> Hello All </h1>
+
+      <button onClick={loadUsers}>get data</button>
+      
+      <h2>Users</h2>
+        <ul>
+        {users.map(({id, email,first_name,last_name})=>(
+          <li key={id}>
+            Name : {first_name} {last_name} Email: {email}        
+          </li>
+        ))}
+      </ul>
+
     </div>
-  );
+  )
 }
-
 export default App;
